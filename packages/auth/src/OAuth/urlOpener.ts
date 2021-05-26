06126718e0e0d4e706486a7653530b9031fb1c51
wlee221 <wlee221@live.unc.edu>
2021-05-25 20:45:04 -0700

    Use location.href to redirect

--- packages/auth/src/OAuth/urlOpener.ts
+++ packages/auth/src/OAuth/urlOpener.ts
@@ -14,10 +14,5 @@
 const SELF = '_self';
 
 export const launchUri = (url: string) => {
-	const windowProxy = window.open(url, SELF);
-	if (windowProxy) {
-		return Promise.resolve(windowProxy);
-	} else {
-		return Promise.reject();
-	}
+	if (window.location) window.location.href = url;
 };
