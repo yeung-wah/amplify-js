db5b0ead97c7250ed1bc9a7dbc4fccaace47f2a8
hkjpotato <hkjpotato@gmail.com>
2021-11-01 14:57:47 -0400

    fix typesVersions

---
+++ packages/api/types/ts3.7/types/index.d.ts
@@ -0,0 +1,9 @@
+import { API } from '../../../lib-esm/API';
+export { API, APIClass } from '../../../lib-esm/API';
+export {
+	graphqlOperation,
+	GraphQLAuthError,
+	GRAPHQL_AUTH_MODE,
+} from '@aws-amplify/api-graphql';
+export type { GraphQLResult } from '@aws-amplify/api-graphql';
+export default API;
